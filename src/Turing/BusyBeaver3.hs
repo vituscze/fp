c53bc4169450f8e2db3367d405dacc178cc9060a
Vít Šefl <vituscze@gmail.com>
2021-08-10 17:13:41 +0200

    use k

--- src/Turing/BusyBeaver3.hs
+++ src/Turing/BusyBeaver3.hs
@@ -18,9 +18,9 @@ initial = pair :. nat 0 :. (triple :. nil :. nat 0 :. nil)
 trans :: Expr
 trans = "state sym" |-> "state" :. ("s" |-> "s" :. (k :. state2) :. state1) :. state0
   where
-    state0 = "sym" :. ("s" |-> triple :. nat 1 :. false :. nat 3) :. (triple :. nat 1 :. false :. nat 1)
-    state1 = "sym" :. ("s" |-> triple :. nat 1 :. false :. nat 1) :. (triple :. nat 0 :. false :. nat 2)
-    state2 = "sym" :. ("s" |-> triple :. nat 1 :.  true :. nat 0) :. (triple :. nat 1 :.  true :. nat 2)
+    state0 = "sym" :. (k :. (triple :. nat 1 :. false :. nat 3)) :. (triple :. nat 1 :. false :. nat 1)
+    state1 = "sym" :. (k :. (triple :. nat 1 :. false :. nat 1)) :. (triple :. nat 0 :. false :. nat 2)
+    state2 = "sym" :. (k :. (triple :. nat 1 :.  true :. nat 0)) :. (triple :. nat 1 :.  true :. nat 2)
 
 end :: Expr
 end = "state" |-> "state" :. ("s" |-> "s" :. ("ss" |-> "ss" :. (k :. true) :. false) :. false) :. false
